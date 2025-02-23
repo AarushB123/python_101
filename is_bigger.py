172fddc61255f8589f495e681943ce41468104eb
aarush.bandaru <aarush.bandaru@gmail.com>
2025-02-23 16:32:23 -0500

    Adding gitignore

---
+++ is_bigger.py
@@ -0,0 +1,16 @@
+x = 10
+y = 15
+if x > y:
+    print(str(x) + " is bigger than " + str(y))
+
+    if x % 2 == 0:
+        print(str(x) + " is even")
+    else:
+        print(str(x) + " is odd")
+else:
+    print(str(y) + " is bigger than " + str(x))
+
+    if y % 2 == 0:
+        print(str(y) + " is even")
+    else:
+        print(str(y) + " is odd")

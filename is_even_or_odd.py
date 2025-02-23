273f6efd4840926ef91faa97e2bdc6ea280fdc39
aarush.bandaru <aarush.bandaru@gmail.com>
2025-02-23 16:11:25 -0500

    wrote an even or odd program

---
+++ is_even_or_odd.py
@@ -0,0 +1,7 @@
+x = 5
+remainder = x % 2
+#print(remainder)
+if remainder == 0:
+    print(str(x) + " is Even")
+else:
+    print(str(x) + " is Odd")
---
+++ test_data_types.py
@@ -0,0 +1,9 @@
+x = '1a'
+print(x, type(x))
+print(x + x)
+x = 1
+print(x, type(x))
+print(x + x)
+x = 1.0
+print(x, type(x))
+
